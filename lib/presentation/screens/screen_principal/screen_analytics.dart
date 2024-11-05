c7992c33501a6ce4e16852d02b91ba378fbf3995
AndreaHerrera20034 <andreaherrera20034@gmail.com>
2024-11-05 17:12:47 -0600

    pantalla de analisis

--- lib/presentation/screens/screen_principal/screen_analytics.dart
+++ lib/presentation/screens/screen_principal/screen_analytics.dart
@@ -9,7 +9,7 @@ class ScreenAnalyticsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Análisis',
+          'Análisis de los datos',
           style: TextStyle(
             fontFamily: 'Montserrat',
             fontSize: 24,
@@ -18,26 +18,98 @@ class ScreenAnalyticsPage extends StatelessWidget {
         ),
         actions: const [],
       ),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Text(
-                  '',
-                  style: TextStyle(
-                    fontFamily: 'ROBOTO',
-                    fontSize: 30,
-                  ),
+                //Botones superiores
+                Wrap(
+                  spacing: 8.0,
+                  runSpacing: 8.0,
+                  alignment: WrapAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(onPressed: () {}, child: const Text('Boton'),),
+                    ElevatedButton(onPressed: () {}, child: const Text('Boton'),),
+                    ElevatedButton(onPressed: () {}, child: const Text('Boton'),)
+                  ],
                 ),
-                
+                const SizedBox(height: 16),
+
+                // Primera gráfica
+                Container(
+                  height: constraints.maxWidth > 600 ? 250 : 150,
+                  color: Colors.grey[300],
+                  alignment: Alignment.center,
+                  child: const Text('Gráfica'),
+                ),
+                const SizedBox(height: 16),
+
+                // Gráficas pequeñas
+                Row(
+                  children: [
+                    Flexible(
+                      child: Container(
+                        height: 100,
+                        color: Colors.grey[300],
+                        alignment: Alignment.center,
+                        child: const Text('Gráfica'),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Flexible(
+                      child: Container(
+                        height: 100,
+                        color: Colors.grey[300],
+                        alignment: Alignment.center,
+                        child: const Text('Gráfica'),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+
+                // Infomración detallada
+                const Text(
+                  'Información detallada',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                Container(
+                  height: 150,
+                  color: Colors.grey[300],
+                  alignment: Alignment.center,
+                  child: const Text('Card con info'),
+                ),
+                const SizedBox(height: 16),
+
+                // Recomendaciones
+                const Text(
+                  'Recomendaciones',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                Wrap(
+                  spacing: 8.0,
+                  runSpacing: 8.0,
+                  alignment: WrapAlignment.spaceEvenly,
+                  children: List.generate(3, (index){
+                    return Container(
+                      width: (constraints.maxWidth - 48) / 3,
+                      height: 100,
+                      color: Colors.grey[300],
+                      alignment: Alignment.center,
+                      child: const Text('Card'),
+                    );
+                  }),
+                ),
               ],
             ),
-          ),
-        ],
-      ),
+          );
+        },
+      )
     );
   }
 }
